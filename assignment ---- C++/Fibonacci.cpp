0a8c5126978ca44a6a78dd3539d3369f565e21fe
Sreedeep Pradeep <pradeepantkmalappattam@gmail.com>
2025-07-19 07:42:33 +0530

    Create Fibonacci.cpp

---
+++ assignment ---- C++/Fibonacci.cpp
@@ -0,0 +1,29 @@
+//program to find the fibinoci In C++
+
+#include<iostream>
+using namespace std;
+
+void Fibonacci(int a){
+    int fib[a],i;
+    for(i=0;i<=a;i++){
+        if(i==0){
+            fib[i]=0;
+        }
+        else if(i == 1){
+            fib[i]=1;
+        }
+        else {
+            fib[i]=fib[i-2]+fib[i-1];
+        }
+        if(fib[i] > a){
+            break; //used to terminate printing the fibinoci after the number entered 
+        }
+        cout<< "[" << fib[i] << "] " << endl;
+    }
+}
+int main(){
+    int num;
+    cout << "enter the number to get factorial" << endl;
+    cin >> num;
+    Fibonacci(num);
+}
