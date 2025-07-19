fb5cdc9e9732af57d1da69326b887867e624aed2
Sreedeep Pradeep <pradeepantkmalappattam@gmail.com>
2025-07-19 15:46:34 +0530

    Update Fibonacci.cpp

--- assignment ---- C++/Fibonacci.cpp
+++ assignment ---- C++/Fibonacci.cpp
@@ -1,27 +1,28 @@
+
 //program to find the fibinoci In C++
 
 #include<iostream>
 using namespace std;
 
-void Fibonacci(int a){
+void Fibonacci(int a) {
     int fib[a],i;
-    for(i=0;i<=a;i++){
-        if(i==0){
+    for(i=0; i<=a; i++) {
+        if(i==0) {
             fib[i]=0;
         }
-        else if(i == 1){
+        else if(i == 1) {
             fib[i]=1;
         }
         else {
             fib[i]=fib[i-2]+fib[i-1];
         }
-        if(fib[i] > a){
-            break; //used to terminate printing the fibinoci after the number entered 
+        if(fib[i] > a) {
+            break; //used to terminate printing the fibinoci after the number entered
         }
         cout<< "[" << fib[i] << "] ";
     }
 }
-int main(){
+int main() {
     int num;
     cout << "enter the number to get fibonnaci number up to that" << endl;
     cin >> num;
