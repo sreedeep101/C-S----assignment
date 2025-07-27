f2f5ec07683be87570287d2a2a6ef5dfa14d916e
Sreedeep Pradeep <pradeepantkmalappattam@gmail.com>
2025-07-27 10:38:18 +0530

    Update substrateMatrix.c

--- assignment ----- IDS/2-D_Array_Problems/substrateMatrix.c
+++ assignment ----- IDS/2-D_Array_Problems/substrateMatrix.c
@@ -1,1 +1,32 @@
+//program to substract two matrix
 
+#include<stdio.h>
+
+#define M 2
+#define N 3
+
+void main() {
+   int a[M][N]={1,0,3,1,2,6};
+   int b[M][N]={3,2,5,6,7,1};
+   int c[M][N];
+   int i,j;
+   
+   //Code to substract this two matrix
+   
+   for(i=0;i<M;i++){
+       for(j=0;j<N;j++){
+           c[i][j]=a[i][j]-b[i][j];
+       }
+   }
+   
+   //code to print the resultant matrix
+   
+   printf("the resultant matrix is:\n");
+   for(i=0;i<M;i++){
+   printf("[ ");
+       for(j=0;j<N;j++){
+           printf("%d ",c[i][j]);
+       }
+   printf("]\n");    
+   }
+}
